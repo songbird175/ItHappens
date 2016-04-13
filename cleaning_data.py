e11920fa6b1d21ad20c7f7f26e105547f3cfbbdf
emilykohler <emily.kohler@students.olin.edu>
2016-04-13 18:52:50 -0400

    Added files via upload

--- cleaning_data.py
+++ cleaning_data.py
@@ -1,13 +1,9 @@
-#  cleaning up data
+# a program to sort the data needed for the map
+# and then put the usable data into a new csv file
 
 import pandas as pd
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt 
 import numpy as np 
-from matplotlib.patches import Polygon
-from matplotlib.pyplot import figure, show
-
-## DATA LISTS
+import csv
 
 def panda_to_list(file_name, title1):
     """
@@ -28,55 +24,25 @@ def panda_to_list(file_name, title1):
         list1.append(col_value)
     return list1
 
-def non_zero_sorting(column_title):
-    """
-    returns a list of information about the college. This program is meannt to 
-    separate the colleges by the number they reported (either zero, 'nan', or nonzero).
-    """
-    # the data converted to a list
-    name = panda_to_list('Public_public.csv', 'Institution name')
-    size = panda_to_list('Public_public.csv', 'Institution Size')
-    unsorted_list = panda_to_list('Public_public.csv', column_title)
-
-    # initialize empty lists for nonzero numbers
-    nonzero_college = []
-    nonzero_size = []
-    nonzero_num = []
-
-    # initialize empty lists for zero numbers
-    zero_college = []
-    zero_size = []
-    zero_num = []
-
-    # separates the unsorted list according to zero versus nonzero
-    for i in range(len(unsorted_list)):
-        if unsorted_list[i] == '0.0' or unsorted_list[i] == 'nan':
-            zero_college.append(name[i])
-            zero_size.append(size[i])
-            zero_num.append(0)
-        else:
-            nonzero_college.append(name[i])
-            nonzero_size.append(size[i])
-            nonzero_num.append(unsorted_list[i])
-    return [nonzero_college, nonzero_size, nonzero_num, zero_college, zero_size, zero_num]
-
-def college_coords(list1):
+def college_coords(name):
     """
     returns the latitude and longitude depending on the list of colleges
     determined by the variable 'list1'. 
     """
     # data containing the college name and colelge coordinates
+    size = panda_to_list('Public_public.csv', 'Institution Size')
+    case = panda_to_list('Public_public.csv', 'Sex offenses - Non-forcible')
     lname = panda_to_list('hd2011.csv', 'INSTNM')
     lat = panda_to_list('hd2011.csv', 'LATITUDE')
     lon = panda_to_list('hd2011.csv', 'LONGITUD')
-    name = non_zero_sorting('Sex offenses - Forcible')[list1]
 
     # initializes empty lists
     lons = []
     lats = []
     coords_list = []
     no_coords = []
-    coords = []
+    sizes = []
+    cases = []
 
     # sorts between the two data lists by checking if
     # the names of the colleges match. If the names of the 
@@ -87,80 +53,27 @@ def college_coords(list1):
         for college2 in lname:
             if college1 == college2:
                 index = lname.index(college2)
+                index2 = name.index(college1)
                 lons.append(float(lon[index]))
                 lats.append(float(lat[index]))
+                sizes.append(float(size[index2]))
+                cases.append(float(case[index2]))
                 coords_list.append(college1)
         if college1 not in coords_list:
             no_coords.append(college1)
-    return [lons, lats]
+    return [coords_list, lons, lats, sizes, cases]
 
+college_info = college_coords(panda_to_list('Public_public.csv', 'Institution name'))
+college_name = college_info[0]
+college_lons = college_info[1]
+college_lats = college_info[2]
+college_size = college_info[3]
+college_case = college_info[4]
 
-## BASEMAP STUFF
-
-fig = plt.figure()
-
-my_map = Basemap(projection='merc', 
-                 lat_0=50,
-                 lon_0=-100,
-                 resolution = 'l', 
-                 area_thresh = 1000.0,
-                 llcrnrlon=-125,
-                 llcrnrlat=24,
-                 urcrnrlon=-67, 
-                 urcrnrlat=50)
-my_map.drawcoastlines()
-my_map.drawcountries()
-my_map.drawmapboundary()
-my_map.drawstates()
-
-my_map.readshapefile('st99_d00', name='states', drawbounds=True)
-
-state_names = []
-for shape_dict in my_map.states_info:
-    state_names.append(shape_dict['NAME'])
-
-ax = plt.gca()
-
-# changes the color of texas to red
-# seg = my_map.states[state_names.index('Texas')]
-# poly = Polygon(seg, facecolor='red',edgecolor='red')
-# ax.add_patch(poly)
-
-ax = plt.gca() # get current axes instance
-
-def point_in_polygon(pt, polygon):
-    """Returns True iff `pt` is inside `polygon`.
-    `polygon` is a list of tuples `(x, y)`."""
-    return matplotlib.path.Path(polygon).contains_point(pt)
-
-def OnClick(event):
-    print "x=%d, y=%d"%(event.x, event.y)
-
-# nonzero
-info = college_coords(0)
-lons = info[0]
-lats = info[1]
-size = non_zero_sorting('Sex offenses - Forcible')[1]
-number = non_zero_sorting('Sex offenses - Forcible')[2]
-
-# zero
-# info = college_coords(3)
-# lons = info[0]
-# lats = info[1]
-# size = non_zero_sorting('Sex offenses - Forcible')[4]
-# number = non_zero_sorting('Sex offenses - Forcible')[5]
-
-x,y = my_map(lons, lats)
-
-for i in range(len(lons)):
-    if float(size[i]) <= 20000:
-        my_map.plot(float(x[i]), float(y[i]), 'go', markersize=float(size[i])/3000)
-    elif float(size[i]) >= 30000:
-        my_map.plot(float(x[i]), float(y[i]), 'ro', markersize=float(size[i])/3000)
-    else:
-        my_map.plot(float(x[i]), float(y[i]), 'bo', markersize=float(size[i])/3000)
-plt.show()
-
-cid_up = fig.canvas.mpl_connect('button_press_event', OnClick)
-
-plt.show()
+# creating columns in the csv file
+my_df = pd.DataFrame({'name' : college_name, 
+                      'size' : college_size,
+                      'lats' : college_lats,
+                      'lons' : college_lons,
+                      'case' : college_case})
+my_df.to_csv('output2.csv', index=False)
