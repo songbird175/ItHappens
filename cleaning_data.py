e88ae0862168f87385371cc4f963c7e651c104ca
emilykohler <emily.kohler@students.olin.edu>
2016-05-04 21:22:07 -0400

    Added files via upload

--- cleaning_data.py
+++ cleaning_data.py
@@ -34,13 +34,15 @@ def college_coords(name):
     of this function can then be put into a csv file for later use.
     """
     # lists from college we have case data for 
-    size = panda_to_list('Public_public.csv', 'Institution Size')
-    case = panda_to_list('Public_public.csv', 'Sex offenses - Forcible')
+    size = panda_to_list('Private_oncampus.csv', 'Institution Size')
+    case1 = panda_to_list('Private_oncampus.csv', 'Sex offenses - Forcible')
+    case2 = panda_to_list('Private_oncampus.csv', 'Sex offenses - Non-forcible')
 
     # lists from college we have coordinates for 
-    names_with_coords = panda_to_list('hd2011.csv', 'INSTNM')
-    lat = panda_to_list('hd2011.csv', 'LATITUDE')
-    lon = panda_to_list('hd2011.csv', 'LONGITUD')
+    names_with_coords = panda_to_list('hd2014.csv', 'INSTNM')
+    lat = panda_to_list('hd2014.csv', 'LATITUDE')
+    lon = panda_to_list('hd2014.csv', 'LONGITUD')
+    state = panda_to_list('hd2014.csv', 'STABBR')
 
     # initializes empty lists
     lons = []
@@ -48,26 +50,33 @@ def college_coords(name):
     coords_list = []
     no_coords = []
     sizes = []
-    cases = []
+    cases1 = []
+    cases2 = []
+    cases3 = []
+    states = []
 
     # Sorts between the two data lists by checking if the names of
     # the colleges match (if college1 == college2). If the names of the 
     # colleges match, that college  can be matched with a latitude 
-    # and longitude. If the names of the colleges don't match, that
-    # college gets added to a list of colleges we need coordinates for.
+    # and longitude. 
     for college1 in name:
         for college2 in names_with_coords:
             if college1 == college2:
-                index = names_with_coords.index(college2)
-                index2 = name.index(college1)
-                lons.append(float(lon[index]))
-                lats.append(float(lat[index]))
-                sizes.append(float(size[index2]))
-                cases.append(float(case[index2]))
+                index2 = names_with_coords.index(college2)
+                index1 = name.index(college1)
+                lons.append(float(lon[index2]))
+                lats.append(float(lat[index2]))
+                states.append((state[index2]))
+                sizes.append(float(size[index1]))
+                cases1.append(float(case1[index1]))
+                cases2.append(float(case2[index1]))
+                cases3.append(float(case2[index1])+float(case1[index1]))
                 coords_list.append(college1)
+
+        # makes a list of colleges still wihtout coordinates
         if college1 not in coords_list:
-            no_coords.append(college1)
-    return [coords_list, lons, lats, sizes, cases]
+            no_coords.append(college1) 
+    return [coords_list, lons, lats, sizes, cases1, cases2, cases3, states, no_coords]
 
 
 def non_zero_sorting(return_what):
@@ -89,6 +98,7 @@ def non_zero_sorting(return_what):
     nonzero_num = []
     nonzero_lats = []
     nonzero_lons = []
+    nonzero_state = []
 
     # initialize empty lists for zero numbers
     zero_college = []
@@ -96,6 +106,7 @@ def non_zero_sorting(return_what):
     zero_num = []
     zero_lats = []
     zero_lons = []
+    zero_states = []
     
     # separates the unsorted list according to zero versus nonzero
     for i in range(len(unsorted_list)):
@@ -117,19 +128,23 @@ def non_zero_sorting(return_what):
         return [zero_college, zero_lons, zero_lats, zero_size, zero_num]
 
 
-info = non_zero_sorting('zero')
+# calls lists that will later be added to the csv
+info = college_coords(panda_to_list('Private_oncampus.csv', 'Institution name'))
 c_name = info[0]
 c_lons = info[1]
 c_lats = info[2]
 c_size = info[3]
-c_case = info[4]
-
-percent_list = []
+c_case1 = info[4]
+c_case2 = info[5]
+c_case3 = info[6]
+c_state = info[7]
+no_c = info[8]
 
 # creates the percent list
+percent_list = []
 for i in range(len(c_size)):
     try:
-        percentage = (float(c_case[i])/float(c_size[i]))*10000
+        percentage = (float(c_case3[i])/float(c_size3[i]))*10000
         percent_list.append(float(percentage))
     except:
         percent_list.append(0.0)
@@ -139,7 +154,11 @@ my_df = pd.DataFrame({'name' : c_name,
                       'size' : c_size,
                       'lats' : c_lats,
                       'lons' : c_lons,
-                      'case' : c_case,
+                      'Forible' : c_case1,
+                      'Non-forcible' : c_case2,
+                      'Combined' : c_case3,
+                      'state' : c_state,
                       'perc' : percent_list})
-my_df.to_csv('output2.csv', index=False)
+# creates the csv, with its name
+my_df.to_csv('private_oncampus_nonforcible_forcible.csv', index=False)
 
